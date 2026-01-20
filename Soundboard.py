6d3317e47f634a4127541070eaf32828f3df4bec
TomaKanagy <toma0kanagy@gmail.com>
2026-01-19 20:40:15 -0600

    Update Soundboard.py description for --add

--- Soundboard.py
+++ Soundboard.py
@@ -12,7 +12,7 @@ def main():
 def command_line():
     #Write -help menu and parse command-line for --add argument
     parser = argparse.ArgumentParser(description= "Digital soundboard with sound bites from various songs 🎵")
-    parser.add_argument("--add", help= "Add your own sound bite via mp3 file link.")
+    parser.add_argument("--add", help= "Add your own sound bite via direct link to an audio file. This can be accomplished by adding the file to your github, then filling the in blanks to this direct link format: https://raw.githubusercontent.com/<your_Github_username>/<your_repository_name>/<branch_name>/<file_name>.<extension_name(like m4a or mp3)>\nMake sure the file is public!")
     args = parser.parse_args()
 
     #adds sound bite to a csv file with format: location, title, link
